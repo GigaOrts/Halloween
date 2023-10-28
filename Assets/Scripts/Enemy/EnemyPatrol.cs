9e05f039b8bdd8b9410e59b01328bf655475681a
Katya <51059059+katyashnaider@users.noreply.github.com>
2023-10-28 12:50:01 +0400

    EnemyPatrol.cs + Prefab enemy

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Halloween.Movement;
+using UnityEngine;
+
+namespace Halloween.Enemy
+{
+    [RequireComponent(typeof(CharacterMovement))]
+    [RequireComponent(typeof(CharacterFlipper))]
+    public class EnemyPatrol : MonoBehaviour
+    {
+        [SerializeField] private float _raycastDistance = 1.0f;
+        [SerializeField] private LayerMask _obstacleLayerMask;
+
+        private CharacterMovement _movement;
+        private CharacterFlipper _flipper;
+
+        private void Awake()
+        {
+            _movement = GetComponent<CharacterMovement>();
+            _flipper = GetComponent<CharacterFlipper>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 direction = _flipper.IsFlippedLeft ? Vector2.left : Vector2.right;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, _raycastDistance, _obstacleLayerMask);
+
+            if (hit.collider) 
+                direction = _flipper.IsFlippedLeft ? Vector2.right : Vector2.left;
+
+            _movement.Move(direction);
+        }
+    }
+}
