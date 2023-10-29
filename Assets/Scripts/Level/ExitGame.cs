280b1ecc0d4646dbcacc7034a98e5e5bc55a61ef
Katya <51059059+katyashnaider@users.noreply.github.com>
2023-10-29 19:03:34 +0400

    Made exit game

---
+++ Assets/Scripts/Level/ExitGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Halloween
+{
+    public class ExitGame : MonoBehaviour
+    {
+        [SerializeField] private GameObject _exitGameScreen;
+        
+        [Space]
+        [SerializeField] private Button _buttonYes;
+        [SerializeField] private Button _buttonNo;
+
+        private void OnEnable()
+        {
+            _buttonYes.onClick.AddListener(OnButtonClickYes);
+            _buttonNo.onClick.AddListener(OnButtonClickNo);
+        }
+
+        private void OnDisable()
+        {
+            _buttonYes.onClick.RemoveListener(OnButtonClickYes);
+            _buttonNo.onClick.RemoveListener(OnButtonClickNo);
+        }
+
+        private void OnButtonClickYes() => 
+            Application.Quit();
+
+        private void OnButtonClickNo() => 
+            _exitGameScreen.SetActive(false);
+    }
+}
