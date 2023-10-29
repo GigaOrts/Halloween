9fd62d7302bb36b5939839e589e845184a2b4daf
GigaOrts <ortsortskhoev95@gmail.com>
2023-10-29 22:07:22 +0300

    code finish buy

---
+++ Assets/Customer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Halloween
+{
+    public class Customer : MonoBehaviour
+    {
+        private Wallet.Wallet _wallet;
+
+        private void Awake()
+        {
+            _wallet = GetComponent<Wallet.Wallet>();
+        }
+
+        private void Update()
+        {
+            if(UnityEngine.Input.GetKeyDown(KeyCode.E))
+            {
+
+            }
+        }
+    }
+}
