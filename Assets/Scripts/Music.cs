289a54575a9039ec850ccb778c66f39e67f9febb
AtennopVer <anton7185569@gmail.com>
2023-10-29 21:23:02 +0300

    Added music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Halloween
+{
+    public sealed class Music : MonoBehaviour
+    {
+        private static Music _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+                return;
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
